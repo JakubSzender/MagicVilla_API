99f83147edf27d565719e745c75f656697bc7796
Jakub Szender <jakubszender@gmail.com>
2023-01-17 18:08:24 +0100

    Created userController

---
+++ MagicVilla_VillaAPI/Controllers/UsersController.cs
@@ -0,0 +1,20 @@
+﻿using MagicVilla_VillaAPI.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVilla_VillaAPI.Controllers
+{
+    [Route("api/UsersAuth")]
+    [ApiController]
+    public class UsersController : Controller
+    {
+        public readonly IUserRepository _userRepo;
+        public UsersController(IUserRepository userRepo)
+        {
+            _userRepo= userRepo;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
