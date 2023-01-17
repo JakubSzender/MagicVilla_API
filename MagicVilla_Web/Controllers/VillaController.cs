49fabecc35da4c1ba0347d959e1379de380d33dc
Jakub Szender <jakubszender@gmail.com>
2023-01-17 13:27:46 +0100

    Created SweetAlert for notifications

--- MagicVilla_Web/Controllers/VillaController.cs
+++ MagicVilla_Web/Controllers/VillaController.cs
@@ -45,9 +45,11 @@ namespace MagicVilla_Web.Controllers
                 var response = await _villaService.CreateAsync<APIResponse>(model);
                 if (response != null && response.IsSuccess)
                 {
+                    TempData["success"] = "Villa created successfully";
                     return RedirectToAction(nameof(IndexVilla));
                 }
             }
+            TempData["error"] = "Error encountered";
             return View(model);
         }
 
@@ -68,12 +70,14 @@ namespace MagicVilla_Web.Controllers
         {
             if (ModelState.IsValid)
             {
+                TempData["success"] = "Villa updated successfully";
                 var response = await _villaService.UpdateAsync<APIResponse>(model);
                 if (response != null && response.IsSuccess)
                 {
                     return RedirectToAction(nameof(IndexVilla));
                 }
             }
+            TempData["error"] = "Error encountered";
             return View(model);
         }
 
@@ -96,9 +100,10 @@ namespace MagicVilla_Web.Controllers
                 var response = await _villaService.DeleteAsync<APIResponse>(model.Id);
                 if (response != null && response.IsSuccess)
                 {
+                    TempData["success"] = "Villa deleted successfully";
                     return RedirectToAction(nameof(IndexVilla));
                 }
-            
+            TempData["error"] = "Error encountered";
             return View(model);
         }
     }
