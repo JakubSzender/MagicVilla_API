7d73d48e47c53bdc0a692596ef9174aa50b0b26a
Jakub Szender <jakubszender@gmail.com>
2023-01-28 16:21:33 +0100

    feat: add caching profile

--- MagicVilla_VillaAPI/Program.cs
+++ MagicVilla_VillaAPI/Program.cs
@@ -55,7 +55,13 @@ builder.Services.AddAuthentication( x =>
       };
 
 });
-builder.Services.AddControllers(option => {
+builder.Services.AddControllers(option =>
+{
+    option.CacheProfiles.Add("Default30",
+        new CacheProfile()
+        {
+            Duration = 30
+        });
     //option.ReturnHttpNotAcceptable = true;
 }).AddNewtonsoftJson().AddXmlDataContractSerializerFormatters();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
